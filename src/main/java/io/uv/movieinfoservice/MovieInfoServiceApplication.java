be034a07f54ce3aa0f9af73604e2fb1469c049de
vamshi <atvamshi@gmail.com>
2019-07-21 22:23:45 -0500

    US****: Committed to my Guru

--- src/main/java/io/uv/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/io/uv/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,11 +3,13 @@ package io.uv.movieinfoservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @Slf4j
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
